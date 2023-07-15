9334d020f737b58df3aa3e347f0e25a59bdf0e1d
Edouard Mignien <61579235+Edouard-M@users.noreply.github.com>
2023-07-15 04:48:19 +0200

    Init Navigation bar
    * init project
    
    * init react packages
    
    * init react packages
    
    * Add navigation bar

--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import LandingPage from './pages/LandingPage';
+import Loader from './components/Loader';
+
+window.onload=()=>{
+  <div id="preloader">
+  </div>
+  }
 
 function App() {
+  
   return (
     <div className="App">
+        <Loader />
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<LandingPage/>} />
+          </Routes>
+        </Router>
     </div>
   );
 }
