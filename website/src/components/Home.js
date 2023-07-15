1cbd79693043c258c931575b242e59d40ca1c5b7
Edouard Mignien <edou17@laposte.net>
2023-07-15 21:04:48 +0200

    Add #Home

---
+++ website/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home-mid'>
+        <div className='home-left'>
+            <div className='home-content-left'>
+                <h1 className='home-name'> Edouard Mignien</h1>
+                <div className='home-me'>I'm a <p className='home-full-stack'> Full-Stack </p> Software Engineer</div>
+            </div>
+        </div>
+        <div className='home-right'>
+            <div className='home-content-right'>
+                <div className='home-right-photo'>
+                    <p>photo here</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
--- website/src/pages/LandingPage.js
+++ website/src/pages/LandingPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import Header from '../components/Header'
+import Home from '../components/Home'
+
+
 
 function LandingPage() {
   return (
-    <div className='landing-page'>
+    <div>
         <Header/>
+        <div className='landing-page'>
+          <section id="home" className='home'>
+            <Home/>
+          </section>
+          <div id="main" className='main'>
+            <section id="about" className='about'>
+            about
+            </section>
+            <section id="projects" className='projects'>
+            projects
+            </section>
+            <section id="resume" className='resume'>
+            resume
+            </section>
+          </div>
+        </div>
     </div>
   )
 }
