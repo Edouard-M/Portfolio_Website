f97ef81849e3322bc5cfb8d5c901e895823c3f63
Edouard Mignien <edou17@laposte.net>
2023-08-04 15:02:54 +0200

    Update data experiences

--- src/data/english/ExperiencesData.js
+++ src/data/english/ExperiencesData.js
@@ -8,38 +8,39 @@ const section = {
 var experience1 = {
     title:"ECE - Engineering School",
     subtitle:"“Ecole Centrale d’Electronique” - 2018 - 2023",
-    text:"Développeur Web Backend de services cloud.",
-    list: [{id: 1, text: "Developpement WEB"}, {id: 2, text: "Programmation Python / Java / C / C++ / HTML / CSS"}, {id: 3, text: "Gestion de projet Agile"}],
+    //text:"WEB development - Languages: Python / Java / C / C++ / HTML / CSS - Gestion de projet Agile",
+    list: [{id: 1, text: "WEB & Software development"}, {id: 2, text: "Languages: Python / Java / C / C++ / HTML / CSS"}, {id: 3, text: "Agile project management"}],
+    //list: [],
     image:"sg.png",
 }
 
 var experience2 = {
     title:"KNU - University Exchange - South Korea",
     subtitle:"Kyungpook National University (경북대학교) - 2021",
-    text:"Notions d’intelligence artificielle - Théorie des graphes appliquée à l’AI - Développement Python - Développement FPGA en Verilog",
+    text:"Basics of artificial intelligence - Graph theory applied to AI - Python development - FPGA development in Verilog",
     list:[],
     image:"sg.png",
 }
 
 var experience3 = {
     title:"Design Thinking Program",
-    subtitle:"San Francisco, United States",
-    text:"Design thinking en immersion à la silicon valley",
+    subtitle:"San Francisco, United States - 2020",
+    text:"Design thinking immersion at silicon valley",
     list:[{id: 1, text: "Creation and exhibition of an interactive artwork"}, {id: 2, text: "Design thinking courses by speakers from Silicon Valley"},],
     image:"sg.png",
 }
 
 var experience4 = {
     title:"Intern - Backend Software Engineer",
-    subtitle:"Société Générale - Paris",
-    text:"Développeur Web Backend de services cloud.",
-    list:[{id: 1, text: "Développement Python"}, {id: 2, text: "Sqlalchemy"}, {id: 3, text: "Flask"}, {id: 4, text: "Jenkins"}, {id: 5, text: "Gestion de bases de données postgreSQL"}, {id: 6, text: "Tests et mocks des api dans une optique de TDD"},],
+    subtitle:"Société Générale - 6 months",
+    text:"Development in production of new functionalities for the 'Database Management Services platform'",
+    list:[{id: 1, text: "Python Backend developer for cloud services"}, {id: 2, text: "Backend Framework: Flask"},  {id: 3, text: "Use of ORM: Sqlalchemy"}, {id: 4, text: "PostgreSQL database management"}, {id: 5, text: "Testing and mocking api from a TDD perspective"},  {id: 6, text: "Production environment: Jenkins & Github actions"},],
     image:"sg.png",
 }
 
 var experience5 = {
     title:"Intern - Python Developer",
-    subtitle:"Airbus Defence and Space - Elancourt",
+    subtitle:"Airbus Defence and Space - 5 months",
     text:"Développer un logiciel de traitement d’images dans le but de reconnaître et d’annoter automatiquement un dataset d’images satellites avec Airbus Defence and Space.",
     list:[{id: 1, text: "Développement Python d’un algorithme de traitement d’images"}, {id: 2, text: "Développement de scripts Python"}, {id: 3, text: "Mise en place d’un algorithme de Deep Learning destiné à la reconnaissance automatique de navires en mer"}, {id: 4, text: "Outils de gestion de projets : Notion, Google Collab"},],
     image:"airbus.png",
@@ -47,7 +48,7 @@ var experience5 = {
 
 var experience6 = {
     title:"Intern - IT Junior Consultant",
-    subtitle:"Wavestone - Paris",
+    subtitle:"Wavestone - 2 months",
     text:"Au sein de la practice “IT & Data Architecture” participation à la refonte du système SI d’une grande entreprise au cours de différentes missions :",
     list:[{id: 1, text: "Mise en œuvre d’un cahier des charges adéquat aux besoins du client"}, {id: 2, text: "Formation sur les bases du SI et des réseaux"}, {id: 3, text: "Autonomie et une capacité d’adaptation au regard des différents clients"}],
     image:"wavestone.png",
--- src/data/french/ExperiencesData.js
+++ src/data/french/ExperiencesData.js
@@ -1,6 +1,6 @@
 const section = {
     section:"Expériences",
-    subtitle1:"Education",
+    subtitle1:"Éducation",
     subtitle2:"Professionnel",
     download_button:"Télécharger CV",
 }
@@ -8,13 +8,13 @@ const section = {
 var experience1 = {
     title:"ECE - Ecole d'ingénieurs",
     subtitle:"“Ecole Centrale d’Electronique” - 2018 - 2023",
-    text:"Développeur Web Backend de services cloud.",
-    list: [{id: 1, text: "Developpement WEB"}, {id: 2, text: "Programmation Python / Java / C / C++ / HTML / CSS"}, {id: 3, text: "Gestion de projet Agile"}],
+    text:"",
+    list: [{id: 1, text: "Développement WEB & logiciel"}, {id: 2, text: "Langages: Python / Java / C / C++ / HTML / CSS"}, {id: 3, text: "Gestion de projet Agile"}],
     image:"sg.png",
 }
 
 var experience2 = {
-    title:"KNU - University Exchange - South Korea",
+    title:"KNU - Échange Universitaire - Corée du Sud",
     subtitle:"Kyungpook National University (경북대학교) - 2021",
     text:"Notions d’intelligence artificielle - Théorie des graphes appliquée à l’AI - Développement Python - Développement FPGA en Verilog",
     list:[],
@@ -22,24 +22,24 @@ var experience2 = {
 }
 
 var experience3 = {
-    title:"Design Thinking Program",
-    subtitle:"San Francisco, United States",
+    title:"Programme sur le web design",
+    subtitle:"San Francisco, États-Unis - 2020",
     text:"Design thinking en immersion à la silicon valley",
-    list:[{id: 1, text: "Creation and exhibition of an interactive artwork"}, {id: 2, text: "Design thinking courses by speakers from Silicon Valley"},],
+    list:[{id: 1, text: "Création et exposition d'une œuvre d'art interactive"}, {id: 2, text: "Cours de design thinking par des intervenants de la Silicon Valley"},],
     image:"sg.png",
 }
 
 var experience4 = {
     title:"Stage - Ingénieur Logiciel Web Backend",
-    subtitle:"Société Générale - Paris",
-    text:"Développeur Web Backend de services cloud.",
-    list:[{id: 1, text: "Développement Python"}, {id: 2, text: "Sqlalchemy"}, {id: 3, text: "Flask"}, {id: 4, text: "Jenkins"}, {id: 5, text: "Gestion de bases de données postgreSQL"}, {id: 6, text: "Tests et mocks des api dans une optique de TDD"},],
+    subtitle:"Société Générale - 6 mois",
+    text:"Développement en production de nouvelles fonctionalitées de la plateforme 'Database Management Services'",
+    list:[{id: 1, text: "Développeur backend Python pour les services cloud"}, {id: 2, text: "Framework Backend: Flask"}, {id: 3, text: "Utilisation d'ORM: Sqlalchemy"}, {id: 4, text: "Gestion de bases de données postgreSQL"}, {id: 5, text: "Tests et mocks des api dans une optique de TDD"},  {id: 6, text: "Environnement de production: Jenkins & Github actions"},],
     image:"sg.png",
 }
 
 var experience5 = {
     title:"Stage - Développeur Python",
-    subtitle:"Airbus Defence and Space - Elancourt",
+    subtitle:"Airbus Defence and Space - 5 mois",
     text:"Développer un logiciel de traitement d’images dans le but de reconnaître et d’annoter automatiquement un dataset d’images satellites avec Airbus Defence and Space.",
     list:[{id: 1, text: "Développement Python d’un algorithme de traitement d’images"}, {id: 2, text: "Développement de scripts Python"}, {id: 3, text: "Mise en place d’un algorithme de Deep Learning destiné à la reconnaissance automatique de navires en mer"}, {id: 4, text: "Outils de gestion de projets : Notion, Google Collab"},],
     image:"airbus.png",
@@ -47,7 +47,7 @@ var experience5 = {
 
 var experience6 = {
     title:"Stage - Consultant Junior IT",
-    subtitle:"Wavestone - Paris",
+    subtitle:"Wavestone - 2 mois",
     text:"Au sein de la practice “IT & Data Architecture” participation à la refonte du système SI d’une grande entreprise au cours de différentes missions :",
     list:[{id: 1, text: "Mise en œuvre d’un cahier des charges adéquat aux besoins du client"}, {id: 2, text: "Formation sur les bases du SI et des réseaux"}, {id: 3, text: "Autonomie et une capacité d’adaptation au regard des différents clients"}],
     image:"wavestone.png",
