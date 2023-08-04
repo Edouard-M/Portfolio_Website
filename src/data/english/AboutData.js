f8d0199ca882995ef2af1e01fb255d4f4d492585
Edouard Mignien <edou17@laposte.net>
2023-08-04 20:18:40 +0200

    Update data

--- src/data/english/AboutData.js
+++ src/data/english/AboutData.js
@@ -3,13 +3,13 @@ var aboutData1 = {
     title:"Fullstack Developer",
     text:"Passionate about programming and algorithms. I love fullstack development because of the capability to create and sublimate great programming structure. During my different internships and personal practices, I acquired different knowledges in software engineering that I'd be delighted to share with you.",
     list: [
-        {title: "Degree:", text: "Master"},
+        {title: "Diploma:", text: "Master Software Engineer"},
         {title: "City:", text: "Paris, France"},
-        {title: "Email:", text: "edouard.mignien@gmail.com"},
+        {title: "", text: ""},
 
-        {title: "Diploma:", text: "Software Engineer"},
         {title: "School:", text: "ECE Paris - Engineering School"},
-        {title: "Phone:", text: "(+33) 6 73 60 98 81"},
+        {title: "Email:", text: "edouard.mignien@gmail.com"},
+        {title: "", text: ""},
     ],
 }
 
--- src/data/french/AboutData.js
+++ src/data/french/AboutData.js
@@ -3,13 +3,13 @@ var aboutData1 = {
     title:"Développeur Fullstack",
     text:"Passionné par la programmation et les algorithmes. J'aime le développement fullstack car il permet de créer et de sublimer de grandes structures de programmation. Au cours de mes différents stages et pratiques personnelles, j'ai acquis différentes connaissances en ingénierie et développement web que je serai ravi de partager avec vous.",
     list: [
-        {title: "Niveau:", text: "Master"},
+        {title: "Diplôme:", text: "Master Ingénieur Logiciel"},
         {title: "Ville:", text: "Paris, France"},
-        {title: "Email:", text: "edouard.mignien@gmail.com"},
+        {title: "", text: ""},
 
-        {title: "Diplôme:", text: "Ingénieur Logiciel"},
-        {title: "Ecole:", text: "ECE Paris - Ecole d'ingénieurs"},
-        {title: "Tél:", text: "(+33) 6 73 60 98 81"},
+        {title: "École:", text: "ECE Paris - Ecole d'ingénieurs"},
+        {title: "Email:", text: "edouard.mignien@gmail.com"},
+        {title: "", text: ""},
     ],
 }
 
