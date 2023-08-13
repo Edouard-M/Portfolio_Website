84d181f4e3c69064048bc223af219c31eeddf383
Edouard-M <edou17@laposte.net>
2023-08-13 19:46:43 +0200

    add href link to scroll

--- src/components/generics/ScrollDown.js
+++ src/components/generics/ScrollDown.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 function ScrollDown() {
   return (
-    <div className="mouse-scroll">
-        <div className="mouse">
-            <div className="mouse-in"></div>
-        </div>
-        <div>
-            <span className="down-arrow-1"></span>
-            <span className="down-arrow-2"></span>
-            <span className="down-arrow-3"></span>
-        </div>
-    </div>
+    <a href="#about">
+      <div className="mouse-scroll">
+          <div className="mouse">
+              <div className="mouse-in"></div>
+          </div>
+          <div>
+              <span className="down-arrow-1"></span>
+              <span className="down-arrow-2"></span>
+              <span className="down-arrow-3"></span>
+          </div>
+      </div>
+    </a>
   )
 }
 
